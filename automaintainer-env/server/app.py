bd86d7331327f66be4a6d29e4e8daca400f8514e
Mahakisore7 <kisore2004@gmail.com>
2026-04-08 00:47:08 +0530

    Final Changes for submission

--- automaintainer-env/server/app.py
+++ automaintainer-env/server/app.py
@@ -52,6 +52,9 @@ async def step_env(request: Request):
         "info": info
     }
 
-def start_server():
+def main():
     import uvicorn
     uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == '__main__':
+    main()
